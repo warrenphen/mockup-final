ed9314ff9140f2fbbe8d2e52352915bed017bd21
ikhakoo <ikhakoo@gmail.com>
2015-05-16 19:24:21 -0400

    adding registrations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ActiveRecord::Base
+	has_many :registrations
+	has_many :users, through: :registrations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :registrations
+  has_many :courses, through: :registrations
+  
 end
