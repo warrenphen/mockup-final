0535dac22a3ce986ed406046c230f1a23589fe23
ikhakoo <ikhakoo@gmail.com>
2015-05-18 15:14:39 -0400

    updating controller actions

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -1,2 +1,36 @@
 class RegistersController < ApplicationController
+
+	def new
+  	@register = Register.new
+  end
+
+  def create
+  	load_course
+  
+  	@register = @course.registers.build(res_params)
+    @register.user = current_user
+
+  	if @register.save
+      redirect_to root_path, notice: 'Registration created successfully. Please check your e-mail for confirmation'
+      # UserMailer.conf_email(current_user).deliver_now
+    else
+      redirect_to root_path notice: 'Registration failed. Please try again'
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+private
+  def res_params
+    params.require(:register).permit(:course_id, :user_id)
+  end
+
+  def load_course
+    @course = Course.find(params[:course_id])
+  end
+
 end
