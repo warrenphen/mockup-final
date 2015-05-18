79d14cbe241eb97278cc8a05a7adbcb3f315138b
ikhakoo <ikhakoo@gmail.com>
2015-05-18 12:13:22 -0400

    removed registrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516232117) do
+ActiveRecord::Schema.define(version: 20150518160747) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -19,13 +19,6 @@ ActiveRecord::Schema.define(version: 20150516232117) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "registrations", force: :cascade do |t|
-    t.integer  "course_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
