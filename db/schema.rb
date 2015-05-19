1780debbb4e5efa4391d27cfa773d67819cfac69
ikhakoo <ikhakoo@gmail.com>
2015-05-19 15:07:17 -0400

    add date to courses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518185419) do
+ActiveRecord::Schema.define(version: 20150519190408) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150518185419) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.date     "date"
   end
 
   create_table "registers", force: :cascade do |t|
