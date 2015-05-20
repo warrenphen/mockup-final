f82ffc3dcfe48f5b76cfdc767e409f08d091fb92
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-05-20 12:31:59 -0400

    Changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,18 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# 5.times do |n|
-#     name  = "Web Development Immersive"
-#     price = "9000"
-#     description = "Everything you need to know to be become a professional web developer and launch your own start-up"
-#     Course.create!(name: name,
-#   	      price: price,
-#   	      description: description
-#   	     )
-#     x = ["|","/","-","+","#"]
-#     print x.shuffle.sample
-# end
-# puts "Course Seed Complete"
+5.times do |n|
+    name  = "Web Development Immersive"
+    price = "9000"
+    description = "Everything you need to know to be become a professional web developer and launch your own start-up"
+    Course.create!(name: name,
+  	      price: price,
+  	      description: description
+  	     )
+    x = ["|","/","-","+","#"]
+    print x.shuffle.sample
+end
+puts "Course Seed Complete"
 
 20.times do |n|
     course = rand(1...5)
