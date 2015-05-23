6f2e260ea4d7b2f0ae38413ccda555be1f9fc423
ikhakoo <ikhakoo@gmail.com>
2015-05-22 23:09:57 -0400

    creating lessons

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521203405) do
+ActiveRecord::Schema.define(version: 20150523030514) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
