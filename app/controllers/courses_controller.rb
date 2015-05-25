58e674e923ca2786171930219f736231e92252b3
ikhakoo <ikhakoo@gmail.com>
2015-05-25 14:43:36 -0400

    updating courses and lessons controllers

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,67 +1,52 @@
 class CoursesController < ApplicationController
-  before_action :set_course, only: [:show, :edit, :update, :destroy]
-
-  # GET /courses
   def index
-    @courses = Course.all
+  @courses = Course.all
   end
 
-  # GET /courses/1
   def show
+    @course = Course.find(params[:id])
+
     if current_user
-      @enrollment = @course.enrollments.build
+      @review = @course.reviews.build
     end
   end
 
-  # GET /courses/new
   def new
     @course = Course.new
   end
 
-  # GET /courses/1/edit
   def edit
+    @course = Course.find(params[:id])
   end
 
-  # POST /courses
   def create
     @course = Course.new(course_params)
 
-    respond_to do |format|
-      if @course.save
-        format.html { redirect_to @course, notice: 'Course was successfully created.' }
-      else
-        format.html { render :new }
-      end
+    if @course.save
+      redirect_to courses_url
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /courses/1
   def update
-    respond_to do |format|
-      if @course.update(course_params)
-        format.html { redirect_to @course, notice: 'Course was successfully updated.' }
-      else
-        format.html { render :edit }
-      end
+    @course = Course.find(params[:id])
+
+    if @course.update_attributes(course_params)
+      redirect_to course_path(@course)
+    else
+      render :edit
     end
   end
 
-  # DELETE /courses/1
   def destroy
+    @course = Course.find(params[:id])
     @course.destroy
-    respond_to do |format|
-      format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
-    end
+    redirect_to courses_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_course
-      @course = Course.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def course_params
-      params.require(:course).permit(:name, :price, :description)
-    end
+  def course_params
+    params.require(:course).permit(:name, :price, :description, :date)
+  end
 end
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,69 +1,46 @@
 class LessonsController < ApplicationController
-  before_action :set_lesson, only: [:show, :edit, :update, :destroy]
+ before_filter :ensure_logged_in, only: [:create, :destroy]
 
-  # GET /lessons
-  def index
-    @lessons = Lesson.all
+  def show 
+    @course = load_course
+    @lesson = Lesson.find(params[:id])
   end
 
-  # GET /lessons/1
-  def show
-  end
-
-  # GET /lessons/new
   def new
-    @lesson = Lesson.new
+    @course = load_course
+    @lesson = @course.lessons.build
   end
 
-  # GET /lessons/1/edit
-  def edit
-  end
-
-  # POST /lessons
   def create
     @course = load_course
-    @lessons = @course.lessons.build(lesson_params)
+    @lesson = @course.lessons.build(lesson_params)
+    @lesson.user = current_user
 
     respond_to do |format|
       if @lesson.save
-        format.html { redirect_to lesson_path(@course.id), notice: 'Lesson was successfully created.' }
+        format.html do
+          redirect_to course_path(@course.id), notice: 'lesson created successfully' 
+        end
+        format.js
       else
-        format.html { render :new }
+        format.html { render 'courses/show', alert: 'There was an error' }
       end
     end
   end
 
-  # PATCH/PUT /lessons/1
-  def update
-    respond_to do |format|
-      if @lesson.update(lesson_params)
-        format.html { redirect_to @lesson, notice: 'Lesson was successfully updated.' }
-      else
-        format.html { render :edit }
-      end
-    end
-  end
-
-  # DELETE /lessons/1
   def destroy
+    @lesson = Lesson.find(params[:id])
     @lesson.destroy
-    respond_to do |format|
-      format.html { redirect_to lessons_url, notice: 'Lesson was successfully destroyed.' }
-    end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_lesson
-      @lesson = Lesson.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def lesson_params
-      params.require(:lesson).permit(:name, :description, :course_id)
-    end
+private
+  def lesson_params
+    params.require(:lesson).permit(:name, :description, :course_id)
+  end
 
-    def load_course
-      @course = Course.find(params[:course_id])
-    end
+  def load_course
+    @course = Course.find(params[:course_id])
+  end
+
 end
