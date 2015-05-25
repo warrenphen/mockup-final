a70eaaf3c9c5be299d779d9c0e4fb1b775471d40
aroswell <aroswell@gmail.com>
2015-05-24 23:11:22 -0400

    changes to markdowns controller.

--- app/controllers/markdowns_controller.rb
+++ app/controllers/markdowns_controller.rb
@@ -2,6 +2,8 @@ class MarkdownsController < ApplicationController
   def show
     page = File.open(File.join(Rails.root, 'lib', 'curriculum', "#{params[:page_id]}.md"), 'r') { |f| f.read }
 
+    # puts page
+
     # initialize a Markdown parser
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true)
 
